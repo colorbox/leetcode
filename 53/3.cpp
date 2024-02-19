2dcce5b89702265b303c1a332aaa9c184613e61d
colorbox <colorbox222@gmail.com>
2024-02-20 00:51:28 +0900

    累積和の形成を統合して1重ループに

---
+++ 53/3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ret = nums[0];
+        vector<int> cums = {0};
+
+        int currentMinCum = cums[0];
+        for(int i=0; i<nums.size(); i++){
+            cums.push_back(cums[i]+nums[i]);
+
+            currentMinCum = min(currentMinCum, cums[i]);
+            ret = max(ret, cums[i+1] - currentMinCum);
+        }
+        return ret;
+    }
+};
