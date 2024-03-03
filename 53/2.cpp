2d85e3a2b152179746a119c340636fd1abd493f9
colorbox <colorbox222@gmail.com>
2024-03-04 02:44:48 +0900

    累積和配列の作成過程を素直な書き方に修正

--- 53/2.cpp
+++ 53/2.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int ret = nums[0];
-        vector<int> cums = {0};
+        vector<int> cums = {};
+        int cum = 0;
         for(int i=0; i<nums.size(); i++){
-            cums.push_back(cums[i]+nums[i]);
+            cums.push_back(cum);
+            cum += nums[i];
         }
-
+        cums.push_back(cum);
+        
         int currentMinCum = cums[0];
         for(int i=0; i<nums.size(); i++){
             currentMinCum = min(currentMinCum, cums[i]);
