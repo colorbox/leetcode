f6be63f1cadde2369c95521b0aed0b8583fd9495
colorbox <colorbox222@gmail.com>
2024-03-14 01:55:30 +0900

    Add 2nd and 3rd codes

---
+++ 21/3.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode ret = ListNode();
+        ListNode *current = &ret;
+
+        while(list1 != nullptr && list2 != nullptr){
+            if(list1->val > list2->val){
+                swap(list1, list2);
+            }
+
+            current->next = list1;
+            current = current->next;
+            list1 = list1->next;
+        }
+
+        if(list1 == nullptr){
+            current->next = list2;
+        }
+        if(list2 == nullptr){
+            current->next = list1;
+        }
+
+        return ret.next;
+    }
+};
