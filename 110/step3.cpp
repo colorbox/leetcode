91c1a93da209fc41aeb3d4fb4e932735e4eebae9
colorbox <colorbox222@gmail.com>
2024-04-14 05:02:51 +0900

    Update

--- 110/step3.cpp
+++ 110/step3.cpp
@@ -1,4 +1,5 @@
 // デフォルト引数を使用
+// isBalancedの再帰部分がゴチャごチャしていて読みづらさを感じたので自身のチェックと再帰を分離
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
@@ -9,7 +10,11 @@ public:
         int left_height = height(root->left);
         int right_height = height(root->right);
 
-        return abs(right_height - left_height) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+        if (abs(right_height - left_height) > 1) {
+            return false;
+        }
+
+        return isBalanced(root->left) && isBalanced(root->right);
     }
 
     int height(TreeNode* root, int base_height = 0) {
