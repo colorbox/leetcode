35ae9476b072cfb796621e185f9bb9a4a40b4ac6
colorbox <colorbox222@gmail.com>
2024-04-18 02:27:13 +0900

    Update method name

--- 110/step5.cpp
+++ 110/step5.cpp
@@ -2,20 +2,20 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        return getHeight(root) != -1;
+        return GetHeight(root) != -1;
     }
 
-    int getHeight(TreeNode* root) {
+    int GetHeight(TreeNode* root) {
         if (!root) {
             return 0;
         }
 
-        int left_height = getHeight(root->left);
+        int left_height = GetHeight(root->left);
         if (left_height == -1) {
             return -1;
         }
 
-        int right_height = getHeight(root->right);
+        int right_height = GetHeight(root->right);
         if (right_height == -1) {
             return -1;
         }
