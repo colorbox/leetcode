bfaa6716054c312d1e8138dd97ec090743ce4448
colorbox <colorbox222@gmail.com>
2024-10-24 21:38:43 +0900

    Add step1 ~ 3

---
+++ 49/step2.cpp
@@ -0,0 +1,19 @@
+/*
+無駄なコピーをなくして空間計算量を改善。
+*/
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<string>> sorted_to_str;
+        for (auto& str: strs) {
+          string key = str;
+          sort(key.begin(), key.end());
+          sorted_to_str[key].push_back(str);
+        }
+        vector<vector<string>>grouped_anagrams;
+        for (auto& [key, group]: sorted_to_str) {
+          grouped_anagrams.push_back(move(group));
+        }
+        return grouped_anagrams;
+    }
+};
